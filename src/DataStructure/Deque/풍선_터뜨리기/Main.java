2c3f1d5dc0e3d5fb79bfda7488be6c4c8eafe45f
unknown <dmstjd365@kw.ac.kr>
2022-01-10 10:44:12 +0900

    백준 2346 풍선 터뜨리기 - Deque

--- src/DataStructure/Deque/풍선_터뜨리기/Main.java
+++ src/DataStructure/Deque/풍선_터뜨리기/Main.java
@@ -6,14 +6,14 @@ import java.util.Deque;
 
 /*
 1. 아이디어
- - 1 ~ n 까지의 풍선을 Deque에 저장
- - Deque이 empty 할 때까지 반복
+ - 1 ~ n 까지의 풍선을 Deque 에 저장
+ - Deque 이 empty 할 때까지 반복
    1) 터뜨린 풍선의 값이 양수 k인 경우
-     - (k-1)개의 풍선을 덱의 First에서 뽑아서 Last로 넣음
-     - 이후, 1개의 풍선을 덱의 First에서 뽑아서 터뜨림
+     - (k-1)개의 풍선을 덱의 First 에서 뽑아서 Last 로 넣음
+     - 이후, 1개의 풍선을 덱의 First 에서 뽑아서 터뜨림
    2) 터뜨린 풍선의 값이 음수 -k인 경우
-     - (k-1)개의 풍선을 덱의 Last에서 뽑아서 First로 넣음
-     - 이후, 1개의 풍선을 덱의 Last에서 뽑아서 터뜨림
+     - (k-1)개의 풍선을 덱의 Last 에서 뽑아서 First 로 넣음
+     - 이후, 1개의 풍선을 덱의 Last 에서 뽑아서 터뜨림
 
 2. 자료구조
  - Deque<Balloon> = new ArrayDeque<>()
@@ -21,7 +21,7 @@ import java.util.Deque;
  - StringBuilder: 출력 값(터뜨리는 풍선 번호 순서) 저장
 
 3. 시간 복잡도
- - while 문으로 Deque이 empty 할 때까지 반복: O(n)
+ - while 문으로 Deque 이 empty 할 때까지 반복: O(n)
  - 터뜨린 풍선 값에 따라 총 (k-1) + 1 = k번 removeXXXX() 반복: O(n)
  => 총 O(n^2)
  => n 최대값 대입: 1,000 x 1,000 = 1,000,000 << 2억 (2초)
@@ -31,8 +31,8 @@ import java.util.Deque;
  - Deque 구현체로 LinkedList 사용하여, 메모리 초과 발생
    => Deque<Balloon> deque = new LinkedList();
    => 원인) LinkedList 는 이중 연결리스트로 메모리량이 많음
- - 단순히 덱의 앞/뒤에서 item을 삽입/삭제 하는 경우,
-   LinkedList 보다는 ArrayDeque을 사용하는 것이 메모리량에서 효율적
+ - 단순히 덱의 앞/뒤에서 item 을 삽입/삭제 하는 경우,
+   LinkedList 보다는 ArrayDeque 을 사용하는 것이 메모리량에서 효율적
 */
 
 class Balloon {
