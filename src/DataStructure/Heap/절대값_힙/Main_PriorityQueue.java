87ca0d431c7df83c0bc4227b378c7111443632db
unknown <dmstjd365@kw.ac.kr>
2022-01-11 12:16:33 +0900

    백준 11286 절댓값 힙 - PriorityQueue 풀이(주석 추가)

--- src/DataStructure/Heap/절대값_힙/Main_PriorityQueue.java
+++ src/DataStructure/Heap/절대값_힙/Main_PriorityQueue.java
@@ -4,6 +4,25 @@ import java.io.*;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
+/*
+- n: 수행해야 할 연산 개수
+- x
+  => x != 0 인 경우, 배열에 x 추가
+  => x == 0 인 경우, 배열에서 최소 절대값 제거 및 출력
+     + 빈 배열이면 0 출력
+
+1. 아이디어
+ - PriorityQueue 에 입력 x 를 추가 또는 삭제
+ - Comparator 또는 람다식으로 PriorityQueue 의 Heap 정렬 기준 명시
+
+2. 자료구조
+ - PriorityQueue<Integer>
+
+3. 시간 복잡도
+ - PriorityQueue 의 시간 복잡도
+   => 삽입 / 삭제: O(log n)
+*/
+
 public class Main_PriorityQueue {
 	static int n;		// 수행할 연산 개수
 	static int x;		// 정수 x
