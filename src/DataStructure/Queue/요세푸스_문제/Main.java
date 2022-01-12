d99cdb9ddcc80267339f4536dbee952dba772150
unknown <dmstjd365@kw.ac.kr>
2022-01-12 16:09:07 +0900

    백준 1158 요세푸스 문제 - Queue

--- src/DataStructure/Queue/요세푸스_문제/Main.java
+++ src/DataStructure/Queue/요세푸스_문제/Main.java
@@ -16,8 +16,12 @@ import java.util.LinkedList;
  - Queue<Integer>: 1 ~ n 명
 
 3. 시간 복잡도
- - O(n * k)
-   => n, k 최대값 대입: 5,000 x 5,000 = 25,000,000 << 2억 (2초)
+ - Queue 에 n명 저장: O(n)
+ - Queue 가 empty 할 때까지 반복: n
+   => 1회 반복에서 Queue 에서 총 k 명 제거: k
+   => 총 반복: O(nk)
+ => 총 시간 복잡도: O(n + nk)
+ => n, k 최대값 대입: 5,000 + 5,000 * 5,000 = 25,005,000 << 2억 (2초)
 */
 
 public class Main {
