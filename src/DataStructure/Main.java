d70ca4bd04f3bb282928ae734bcc3e0647cc8192
Kim EunSung <dmstjd365@naver.com>
2022-01-14 15:11:30 +0900

    .

--- src/DataStructure/Main.java
+++ src/DataStructure/Main.java
@@ -1,2 +1,36 @@
-package DataStructure;public class Main {
+package DataStructure;
+import java.io.*;
+import java.util.StringTokenizer;
+
+/*
+- 특정 건물을 가장 빨리 지을 수 있는 최소 시간 구하기
+- 특정 건물을 짓기 위한 순서 변동 가능
+- 특정 건물을 짓기 위해 다른 특정 건물들이 완성되어야 함
+
+1. 아이디어
+
+
+2. 자료구조
+
+
+3. 시간 복잡도
+
+*/
+
+public class Main {
+	static int t;				// 테스트 케이스 개수
+	static int n;				// 건물의 개수 (1 ~ n번 건물)
+	static int k;				// 건물 순서 규칙의 총 개수
+	static int[] timeArr;		// 입력 D1 ~ Dn, 각 건물당 필요 건설 시간
+	static int x, y;			// 건설 순서, "x 완성 이후 y 건설 시작 가능"
+	static int w;				// 건설 목표 건물 번호
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader(System.in)
+		);
+		StringTokenizer st;
+
+
+	}
 }
