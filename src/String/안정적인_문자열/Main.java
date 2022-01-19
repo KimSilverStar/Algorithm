bdf5823f633691045d658154f5f6dc4b5ad7718a
Kim EunSung <dmstjd365@naver.com>
2022-01-19 13:56:04 +0900

    백준 4889 안정적인 문자열 - String, Stack

--- src/String/안정적인_문자열/Main.java
+++ src/String/안정적인_문자열/Main.java
@@ -1,6 +1,6 @@
 package String.안정적인_문자열;
 import java.io.*;
-import java.util.*;
+import java.util.Stack;
 
 /*
 - 안정적인 문자열을 만들기 위한 최소 연산 횟수 구하기
@@ -12,38 +12,61 @@ import java.util.*;
 - 연산
   1) 여는 괄호 -> 닫는 괄호 변환
   2) 닫는 괄호 -> 여는 괄호 변환
+*/
 
+/*
 1. 아이디어
- - Stack 으로 괄호 매칭
-   => 매칭되는 괄호 쌍들은 제거하고, 매칭 안되는 괄호들만 남김
-   => 남은 괄호들에 대해 BFS 수행
+*** 괄호 문제 => Stack 활용
+
+ - 입력 문자열에서 한 문자씩 확인
+ 1) 여는 괄호 { 는 Stack 에 push
+ 2) 닫는 괄호 }
+   - Stack 이 비어있지 않은 경우 (여는 괄호 존재)
+     => Stack 에서 pop 하여 여는 괄호 + 닫는 괄호 매칭시킴
+   - Stack 이 비어있는 경우
+     => Stack 에 해당 닫는 괄호 } 를 여는 괄호 { 로 바꿔서 push,
+        연산 횟수 + 1
+ 3) 입력 문자열에서 모든 문자 확인 종료
+   - Stack 에는 짝수 개의 여는 괄호 { 만 존재
+     => 여는 괄호 {: 입력 문자열에서 원래 여는 괄호 {
+        or 입력 문자열에서 닫는 괄호 } 였는데 Stack 으로 변환되서 들어온 경우
+   - 연산 횟수 += (Stack 의 size / 2)
+     => Stack 에 남은 여는 괄호들 중, 그 중 절반만 닫는 괄호로 바꾸면
+        남은 모든 괄호들 매칭이 완료됨
 
 2. 자료구조
  - String: 입력 문자열
  - Stack<Character>: 괄호 쌍 매칭
- - Queue<String>, LinkedList<String>: BFS 수행
 
 3. 시간 복잡도
-
+ - 한 테스트 케이스 (문자열) 당 O(len)
+   => 한 문자열 당 최대 2,000
 */
 
 public class Main {
-	static String str;			// 입력 문자열
-	static int minCount;		// 출력 값, 최소 연산 횟수
+	static String str;					// 입력 문자열
+	static Stack<Character> stack;
 
-	static Stack<Character> stack = new Stack<>();
-	static Queue<String> queue = new LinkedList<>();
+	static int solution(String str) {
+		stack = new Stack<>();
+		int minCount = 0;				// 최소 연산 횟수
 
-	/* str 에서 매칭되는 괄호를 제거하고 남은 불안정한 문자열 (매칭 안된 괄호들) 반환 */
-	static String getUnstableString(String str) {
 		for (int i = 0; i < str.length(); i++) {
-			stack.push(str.charAt(i));
-			if (stack.size() < 2)
-				continue;
-
-			char current = stack.pop();
-//			if (stack.peek() == '{')
+			if (str.charAt(i) == '{')
+				stack.push(str.charAt(i));
+			else {        // 닫느 괄호 '}'
+				if (!stack.isEmpty())
+					stack.pop();		// 매칭되는 여는 괄호 { 제거
+				else {
+					// 닫는 괄호 } 를 여는 괄호 { 로 변환하여 추가
+					stack.push('{');
+					minCount++;
+				}
+			}
 		}
+
+		minCount += (stack.size() / 2);
+		return minCount;
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -54,17 +77,12 @@ public class Main {
 
 		int i = 1;
 		while (true) {
-			minCount = 0;
 			str = br.readLine();
-			if (str.charAt(0) == '-')
+			if (str.contains("-"))
 				break;
 
-			sb.append(i).append(". ");
-
-
-
-
-
+			sb.append(i).append(". ")
+					.append(solution(str)).append("\n");
 			i++;
 		}
 
