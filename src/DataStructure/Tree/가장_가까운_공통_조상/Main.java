b6cdb12cc6ff6b550d8863be61ab977c6c8f3a38
Kim EunSung <dmstjd365@naver.com>
2022-02-07 16:03:28 +0900

    백준 3584 가장 가까운 공통 조상 - Tree, DFS, DP, 최소 공통 조상(LCA)

--- src/DataStructure/Tree/가장_가까운_공통_조상/Main.java
+++ src/DataStructure/Tree/가장_가까운_공통_조상/Main.java
@@ -5,20 +5,22 @@ import java.util.*;
 /*
 1. 아이디어
  - 루트 노드 찾기
-   => 트리 노드 정보 입력할 때, 자식 노드들을 체크
+   => 입력 트리 노드 정보가 "부모 노드 - 자식 노드" 형태로 주어짐
+   => 트리 노드 정보 입력할 때, 자식 노드들을 방문 처리
    => 입력 끝난 후, 체크되지 않은 노드가 루트 노드
 
  *** 최소 공통 조상 (Lowest Common Ancestor, LCA) 알고리즘 ***
- 1) 모든 노드의 depth, 직계 부모(2^0 번째 부모) 저장
-   - DFS 수행
+ 1) 모든 노드의 깊이, 2^0 번째 부모 (직계 부모) 저장
+   - DFS 수행: void dfs(int parentNode, int depth)
  2) 모든 노드의 2^i 번째 부모 저장
-   - DP 수행, Sparse Table 에 저장
-   - int[][] parent = new int[n][21];	(n: 트리 노드 개수, 21: 트리 깊이 20이면 충분)
+   - DP 수행: void setParent()
+   - DP 배열 (Sparse Table): int[][] parent = new int[n][21];
+     => n: 트리 노드 개수, 21: 트리 깊이 20이면 충분
      => parent[nodeIdx][i]: 노드의 2^i 번째 부모
-     => parent[nodeIdx][i+1] == parent[ parent[nodeIdx][i] ][i]
-        : 노드의 2^(i+1) 번째 부모 == (노드의 2^i 번째 부모)의 2^i 번째 부모
-          원리) 2^(i+1) = 2 x 2^i = 2^i + 2^i
- 3) 두 노드 n1, n2 의 depth 를 동일하게 맞춤
+  - DP 점화식: parent[nodeIdx][i] == parent[ parent[nodeIdx][i-1] ][i-1]
+    => 노드의 2^(i+1) 번째 부모 == (노드의 2^i 번째 부모)의 2^i 번째 부모
+    => 원리) 2^(i+1) = 2 x 2^i = 2^i + 2^i
+ 3) 두 노드 n1, n2 의 깊이를 동일하게 맞춤
    - 노드 n2 가 더 깊은 노드가 되도록, 필요시 n1 과 n2 를 swap
    - 노드 n2 의 depth 가 n1 과 같아지도록, 부모 방향으로 거슬러 올라감
  4) 두 노드 n1, n2 의 부모가 같아질 때까지,
@@ -27,23 +29,29 @@ import java.util.*;
  !!! 3), 4) 과정에서 노드를 부모 방향으로 이동 시
     - 2^k 칸씩 이동
     - 이동 가능한 최대 칸 부터 이동
-      e.g. 13칸 위로 이동해야 하는 경우: 8칸 => 4칸 => 2칸 => 1칸 씩 이동
+      e.g. 15칸 위로 이동해야 하는 경우: 8칸 => 4칸 => 2칸 => 1칸 씩 이동
 
 2. 자료구조
  - List<Integer>[], ArrayList<Integer>[]: 인접 리스트
  - boolean[]: 노드 방문 확인
    => DFS 로 각 노드의 depth, 직계 부모(2^0 번째 부모) 저장
- - int[][]: 각 노드의 2^i 번째 부모 저장한 Sparse Table (DP 배열)
+ - int[] depth: 각 노드의 깊이
+ - int[][] parent = new int[n][21];
+   => Sparse Table (DP 배열): 각 노드의 2^i 번째 부모 저장
+      ex) parent[node][2]: node 의 2^2 번째 부모
+   => n: 트리의 노드 개수, 21: 깊이 21 이면 왠만한 트리 커버
+   => 메모리: 4 x n x 21 byte
+      n 최대값 대입: 840,000 byte = 0.84 MB
 
 3. 시간 복잡도
  1) DFS 1번 수행: O(V + E) = O(n + n-1) ~= O(2n)
    - n 최대값 대입: 2 x 10^4
  2) DP 수행 (노드의 2^i 번째 부모 값 채우기): O(Max Node Level x n)
-   - n, MAX_LEVEL 최대값 대입: (2 x 10^4) x 20 = 4 x 10^5
+   - n, MAX_LEVEL 최대값 대입: 20 x 10^4 = 2 x 10^5
  3) LCA 수행: O(log n)	(n: 트리의 노드 개수)
    - n 최대값 대입: 대충 log 2^13 = 13		=> 무시 될 정도
  => 최소 공통 조상을 출력하는 1개의 Query (테스트 케이스)에 대해,
-    시간 복잡도 = (2 x 10^4) + (4 x 10^5)
+    시간 복잡도 = (2 x 10^4) + (2 x 10^5)
 */
 
 public class Main {
@@ -57,10 +65,10 @@ public class Main {
 
 	static boolean[] check;				// DFS 노드 방문 확인 (depth 계산한 노드 방문 처리)
 	static final int MAX_LEVEL = 21;
-	static int[] depth;					// DP 배열, 각 노드의 depth
+	static int[] depth;					// 각 노드의 depth
 	static int[][] parent;				// DP 배열, parent[nodeIdx][i]: 노드의 2^i 번째 부모
 
-	/* DP 배열 depth[] 에 각 노드의 깊이 저장, 직계 부모(2^0 번째 부모) 저장 */
+	/* 각 노드의 깊이 저장, 직계 부모(2^0 번째 부모) 저장 */
 	static void dfs(int parentNode, int d) {
 		check[parentNode] = true;
 		depth[parentNode] = d;
@@ -111,6 +119,7 @@ public class Main {
 			if (parent[n1][i] == parent[n2][i])
 				continue;
 
+			// 두 노드의 2^i 번째 부모가 서로 다르면, 2^i 칸씩 위로 올림
 			n1 = parent[n1][i];
 			n2 = parent[n2][i];
 		}
