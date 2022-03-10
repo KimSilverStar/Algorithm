89d6ffef990152ddce5708312526c7df4600ccaf
Kim EunSung <dmstjd365@naver.com>
2022-03-10 22:35:20 +0900

    백준 1389 케빈 베이컨의 6단계 법칙 - 플로이드-와샬

--- src/Shortest_Path/Floyd_Warshall/케빈_베이컨의_6단계_법칙/Main.java
+++ src/Shortest_Path/Floyd_Warshall/케빈_베이컨의_6단계_법칙/Main.java
@@ -10,21 +10,20 @@ import java.util.StringTokenizer;
  - [i]번 노드의 케빈 베이컨 수 = [i]행 dist[i]의 원소 합
  - 친구 관계 O or 친구 관계 X
    => 간선의 가중치 0 or 1
-   => INF = 노드 최대 개수 100 x 간선 가중치 최대값 1 = 100
 
  1) 비용 배열 초기화
-   - 노드 본인 -> 노드 본인은 0 으로 초기화
-     => dist[i][i] = 0
-   - 나머지는 INF 로 초기화
-     => INF 값 = (노드 V 최대 개수) x (각 간선의 가중치 최대값)
+   - dist[][] 를 모두 INF 로 초기화, dist[i][i] = 0
+   - INF = (노드 최대 개수 100) x (간선 가중치 최대값 1) = 100
  2) 비용 배열에 간선 가중치 저장
    - 노드 i -> 노드 j 로 가는 간선이 여러 개일 경우, 간선 가중치 작은 값으로 저장
- 3) 3중 for문
+ 3) 3중 for 문
    - 중간 경유 노드, 시작 노드, 끝 노드
+   - 중간 경유 노드를 거쳐서 갈 때 비용이 더 작은 경우, 비용 갱신
 
 
 2. 자료구조
  - int[][] dist: 비용 배열
+   => 비용 최대값 INF = 100 << 21억 이므로, int 가능
 
 
 3. 시간 복잡도
@@ -34,11 +33,23 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static int n, m;				// 유저(노드) 수 n, 친구 관계(간선)의 수 m
-	static int minNode;				// 출력, 비용 합이 최소인 노드 번호
+	static int minNode = 1;				// 출력, 비용 합이 최소인 노드 번호
 
 	static final int INF = 100;
 	static int[][] dist;			// 비용 배열
 	static int[] numbers;			// 각 유저의 케빈 베이컨 수
+
+	static void solution() {
+		// 각 유저의 케빈 베이컨 수 계산 및 최소인 유저 찾기
+		numbers = new int[n + 1];		// 각 유저의 케빈 베이컨 수 계산
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++)
+				numbers[i] += dist[i][j];
+
+			if (numbers[minNode] > numbers[i])
+				minNode = i;
+		}
+	}
 
 	static void floyd() {
 		// 3) 3중 for문 - 중간 경유 노드, 시작 노드, 끝 노드
@@ -84,19 +95,7 @@ public class Main {
 		}
 
 		floyd();
-
-		numbers = new int[n + 1];		// 각 유저의 케빈 베이컨 수 계산
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= n; j++)
-				numbers[i] += dist[i][j];
-		}
-
-		minNode = 1;					// 케빈 베이컨 수가 최소인 유저 찾기
-		for (int i = 1; i <= n; i++) {
-			if (numbers[minNode] > numbers[i])
-				minNode = i;
-		}
-
+		solution();
 		System.out.println(minNode);
 	}
 }
