dbbfa864cc55362481697e94b60680ed4f13dc44
Kim EunSung <dmstjd365@naver.com>
2022-03-30 17:50:06 +0900

    백준 1105 팔 - Greedy

--- src/Greedy/팔/Main2.java
+++ src/Greedy/팔/Main2.java
@@ -12,6 +12,7 @@ import java.util.StringTokenizer;
      e.g. 800, 899 => 1 (백의 자리 8)
      	  8808, 8880 => 2 (천의 자리 8, 백의 자리 8)
      	  1208, 1288 => 0 (십의 자리 비교 했을 때, 서로 다르므로 비교 종료)
+     	  88083, 88084 => 3 (만의 자리 8, 천의 자리 8, 백의 자리는 0으로 같으므로 비교 계속, 십의 자리 8)
 
 2. 자료구조
 
@@ -22,26 +23,23 @@ import java.util.StringTokenizer;
 */
 
 public class Main2 {
-	static int L, R;				// 1 <= L <= R (최대 20억)
+	static String L, R;				// 1 <= L <= R (최대 20억)
 	static int minCount;			// 가장 적은 8 개수
 
 	static void solution() {
-		String strL = String.valueOf(L);
-		String strR = String.valueOf(R);
-
 		// 1) L 의 전체 자릿 수 != R 의 전체 자릿 수인 경우
-		if (strL.length() != strR.length()) {
+		if (L.length() != R.length()) {
 			minCount = 0;
 			return;
 		}
 
 		// 2) L 의 전체 자릿 수 == R 의 전체 자릿 수인 경우
-		for (int i = 0; i < strL.length(); i++) {
+		for (int i = 0; i < L.length(); i++) {
 			// 서로 자릿 수 값이 다르면, 비교 종료
-			if (strL.charAt(i) != strR.charAt(i))
+			if (L.charAt(i) != R.charAt(i))
 				break;
 			// 각 동일 자릿 수를 비교하여, 8로 같은 자릿 수의 개수
-			else if (strL.charAt(i) == '8' && strR.charAt(i) == '8')
+			else if (L.charAt(i) == '8' && R.charAt(i) == '8')
 				minCount++;
 		}
 	}
@@ -52,8 +50,8 @@ public class Main2 {
 		);
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		L = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());
+		L = st.nextToken();
+		R = st.nextToken();
 
 		solution();
 		System.out.println(minCount);
