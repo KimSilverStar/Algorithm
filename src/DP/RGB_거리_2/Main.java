1a8b614c71206e485a7b30d1d23283079c8e03c2
Kim EunSung <dmstjd365@naver.com>
2022-04-22 15:34:25 +0900

    백준 17404 RGB 거리 2 - DP

--- src/DP/RGB_거리_2/Main.java
+++ src/DP/RGB_거리_2/Main.java
@@ -14,10 +14,11 @@ import java.util.StringTokenizer;
    - 집의 색 j: Red 0, Green 1, Blue 2
 
  2) 규칙 및 점화식
-   - [1]번 집의 색을 R, G, B 로 고정하고, 나머지 집의 색을 차례로 지정
+   - [1]번 집과 마지막 [n]번 집의 색이 달라야 함
+     => [1]번 집의 색을 R, G, B 중 1개로 고정하고, 나머지 [2]번 ~ [n]번 집의 색을 차례로 지정
    - 초기식: [1]번 집 색칠
      => 고정 색깔 제외, 나머지 2개 색은 INF 로 초기화
-     ex) [1]번 집 색이 R [0] 이면
+     ex) [1]번 집을 R [0] 로 색칠하면
      	 dp[1][0] = costs[1][0], 나머지 2개 색 dp[1][1] = INF, dp[1][2] = INF
    - 점화식: [i]번 집 색칠
      => 이전 집의 색과 다른 2개 색 중에서 최소값 선택
@@ -35,11 +36,11 @@ import java.util.StringTokenizer;
 2. 자료구조
  - int[][] dp: DP 배열
    ① 자료형: 최대값 10^3 x 10^3 = 10^6 << 21억 이므로, int 가능
-   ② 메모리: 대략 최대 10^3 x 3 byte = 3 KB
+   ② 메모리: 대략 최대 4 x 10^3 x 3 byte = 12 KB
 
 
 3. 시간 복잡도
- - DP 배열 채우기: O(3 x n) = O(3 x n)
+ - DP 배열 채우기: O(3 x n)
    => n 최대값 대입: 3 x 10^3 << 0.5억
 */
 
@@ -47,12 +48,12 @@ public class Main {
 	static int n;						// 집 개수
 	static int[][] costs;				// 각 집의 R, G, B 색칠 비용
 	static int[][] dp;
-	static final int INF = 1_000_000;	// (n 최대값 10^3) x (비용 최대값 10^3)
-	static int minCost = INF;			// 모든 집을 칠하는 최소 비용
+	static final int INF = 1_000_000;	// DP 배열 원소 최대값 (최대 누적 비용) = 10^3 x 10^3
+	static int minCost = INF;			// 출력, 모든 집을 색칠하는 최소 비용
 
 	static void solution() {
-		// [1]번 집의 색 고정
 		for (int firstColor = 0; firstColor < 3; firstColor++) {
+			// [1]번 집의 색 고정
 			if (firstColor == 0) {
 				dp[1][0] = costs[1][0];
 				dp[1][1] = INF;
@@ -75,11 +76,12 @@ public class Main {
 				dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + costs[i][1];
 				dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + costs[i][2];
 
-				// 마지막 [n]번 집에서 [1]번 집의 색은 제외
+				// 마지막 [n]번 집의 색 != [1]번 집의 색
 				if (i == n)
 					dp[i][firstColor] = INF;
 			}
 
+			// 모든 집을 칠하는 최소 비용 minCost 갱신
 			for (int j = 0; j < 3; j++)
 				minCost = Math.min(minCost, dp[n][j]);
 		}
@@ -92,7 +94,7 @@ public class Main {
 		StringTokenizer st;
 
 		n = Integer.parseInt(br.readLine());
-		costs = new int[n + 1][3];			// [1][0] ~ [n][3] 사용
+		costs = new int[n + 1][3];			// [1][] ~ [n][] 사용
 		dp = new int[n + 1][3];
 		for (int i = 1; i <= n; i++) {
 			st = new StringTokenizer(br.readLine());
